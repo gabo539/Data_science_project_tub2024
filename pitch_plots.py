e7a27db91056919c6125caef4f7ef6ed120d13bb
Solana Neumeier <solana.neumeier@tu-berlin.com>
2024-06-12 16:25:42 +0200

    kleine änderungen

--- pitch_plots.py
+++ pitch_plots.py
@@ -10,18 +10,23 @@ first_4_columns =wwrem_p_gdp.iloc[:, 0:4]
 #Limitation to the intervall from 1990 to 2020
 selected_rows = first_4_columns[(first_4_columns['Year'] >= 1990) & (first_4_columns['Year'] <= 2020)]
 
+
 #Summation of all remittances of all countries on each seperate year
-sum_per_year = selected_rows.groupby('Year')['Personal remittances, received (% of GDP)'].sum()
+sum_per_year = selected_rows.groupby('Year')['Personal remittances, received (% of GDP)'].mean()
+
 
 #Plot 1
 plt.figure(figsize=(10, 6))
 plt.plot(sum_per_year.index, sum_per_year.values, marker='o', linestyle='-')
 plt.xlabel('Year')
-plt.ylabel('Sum of Remittances')
-plt.title('Sum of Remittances Over Years')
+plt.ylabel('Sum of Remittances, received (% of GDP)')
+plt.title('Sum of Remittances, received (% of GDP) Over Years')
 plt.grid(True)
 plt.tight_layout()
 plt.show()
+
+
+
 
 
 
